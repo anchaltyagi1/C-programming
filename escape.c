7c44195c5e90b5e8e64cbb290b8a774fe336786d
anchaltyagi1 <139471943+anchaltyagi1@users.noreply.github.com>
2023-08-22 20:56:12 +0530

    Escape Sequences

---
+++ escape.c
@@ -0,0 +1,14 @@
+// In this program we are using escape sequences.
+
+#include <stdio.h>
+int main()
+{
+// using \n for new line.
+    printf("Hey,\n My name is Anchal Tyagi.I'm pursuing MCA from KIET. \n\n"); 
+//using \t for some space.
+      printf("Hey,\t My name is Anchal Tyagi.I'm pursuing MCA from KIET.\n\n"); 
+//using \\ for adding / in the text.
+        printf("Hey,\\ My name is Anchal Tyagi.I'm pursuing MCA from KIET.\n\n"); 
+
+        return 0;
+}
