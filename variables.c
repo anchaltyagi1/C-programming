fe8faa97e7ff6e9ebb95a9b0cc88f8d890664170
anchaltyagi1 <139471943+anchaltyagi1@users.noreply.github.com>
2023-08-23 13:13:13 +0530

    Variables

---
+++ variables.c
@@ -0,0 +1,13 @@
+// Create variables
+#include <stdio.h>
+int main(){
+int numb= 15;            // Integer (whole number)
+float floatnumb= 5.99;   // Floating point number
+char letter = 'D';       // Character
+
+// Print variables
+printf(" %d \n ", numb);
+printf(" %f \n", floatnumb);
+printf(" %c \n", letter);
+return 0;
+}
